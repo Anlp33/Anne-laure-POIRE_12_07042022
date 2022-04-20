92e5a3e1eb5aceabed26653225dde7098c4ddd0a
Anne-laure POIRÉ <82209328+Anlp33@users.noreply.github.com>
2022-04-20 17:27:25 +0800

    Add files via upload

---
+++ src/urlAPI.js
@@ -0,0 +1,15 @@
+// export const urlAPI = {
+
+//     userMainData(id) {
+//         return `http://localhost:3000/user/${id}`;
+//     },
+//     userActivity(id) {
+//         return `/user${id}-data/Information.json`;
+//     },
+//     userPerformance(id) {
+//         return `/user${id}-data/Information.json`;
+//     },
+//     userAverageSession(id) {
+//         return `/user${id}-data/Information.json`;
+//     },
+// };
---
+++ src/urlMockData.js
@@ -0,0 +1,16 @@
+export const urlMockData = {
+
+    userMainData(id) {
+        return `/user${id}-data/Information.json`;
+    },
+    userActivity(id) {
+        return `/user${id}-data/Activity.json`;
+    },
+    userPerformance(id) {
+        return `/user${id}-data/Performance.json`;
+    },
+    userAverageSession(id) {
+        return `/user${id}-data/AverageSession.json`;
+    },
+};
+
