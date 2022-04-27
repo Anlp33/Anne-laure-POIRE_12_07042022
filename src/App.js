e93be63c67b0533d2a4e19e5b815512129d0959c
Anne-laure POIRÉ <82209328+Anlp33@users.noreply.github.com>
2022-04-27 16:23:08 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "./styles/index.css";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
-// import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import { Routes, Route } from "react-router-dom";
+import { DataProvider } from "./utils/context";
 
 function App() {
   return (
-    <div>
+    <DataProvider>
       <Header />
-      <Dashboard />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="dashboard/:id" element={<Dashboard />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </DataProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import './styles/header.css';
+import "./styles/cards.css";
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
