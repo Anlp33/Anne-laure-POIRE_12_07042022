15d1df20827641d678062dfc5154f0a63d2e823c
Anne-laure POIRÉ <82209328+Anlp33@users.noreply.github.com>
2022-05-03 23:30:23 +0800

    remove reportweb error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import './styles/header.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import "./styles/header.css";
 import "./styles/cards.css";
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -15,9 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
