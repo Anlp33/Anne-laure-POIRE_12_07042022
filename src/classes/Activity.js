8eb2d526036fa5f6c0d3236d710fbc50bf51eb5a
Anne-laure POIRÉ <82209328+Anlp33@users.noreply.github.com>
2022-05-09 18:10:59 +0800

    Delete src/classes directory

--- src/classes/Activity.js
+++
@@ -1,7 +0,0 @@
-class Activity {
-    constructor(userId, sessions) {
-        this.userId = userId
-        this.sessions = sessions
-    }
-}
-export default Activity
--- src/classes/Performance.js
+++
@@ -1,8 +0,0 @@
-class Performance {
-  constructor(userId, kind, data) {
-    this.userId = userId;
-    this.kind = kind;
-    this.data = data;
-  }
-}
-export default Performance;
